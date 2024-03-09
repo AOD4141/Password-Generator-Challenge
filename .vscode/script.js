b8e8c339b73a7eac2365f7215f631009446befa7
Adebayo <adenguk5251@gmail.com>
2024-03-09 16:35:49 +0000

    added javascript code to make the password generator work in console.log

--- .vscode/script.js
+++ .vscode/script.js
@@ -4,19 +4,6 @@
 
 //creating a password generator function
 
-var passwordresult = document.getElementById("passwordresult");
 
-var passwordGenerator = () => {
-    var passwordLength = 8;
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-    var password = "";
 
-    for (var i= 0; i <= passwordLength; i++) {
-        var randomNumber = Math.floor(Math.random()* charset.length);
-    password += charset.substring(randomNumber, randomNumber +1);
-    }
-   
-    document.getElementById("passwordresult").value = password;
-}
 
-console.log(passwordGenerator())
