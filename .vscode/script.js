d17f9308b398ad7c83254597b2d7ef2c898b2c5b
Adebayo <adenguk5251@gmail.com>
2024-03-14 23:03:42 +0000

    use the for loop function to set what passwordgenerator function will read

--- .vscode/script.js
+++ .vscode/script.js
@@ -3,3 +3,10 @@ function passwordGenerator ()  {
     var passwordLength = 8;
     var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
     var password = "";
+    for (var i= 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random()* charset.length);
+    password += charset.substring(randomNumber, randomNumber +1);
+    }
+    
+    return password;
+    }
