ff4374839fedd88abe86fe8a127994ea372fda6c
Adebayo <adenguk5251@gmail.com>
2024-03-14 22:55:14 +0000

    added the existing template for index.HTML

--- .vscode/secondscript.js
+++ .vscode/secondscript.js
@@ -1,23 +1,23 @@
 
 
-let passwordGenerator = () => {
-    // setting password length
-    let length = 8;
-    // creating character set
-    let charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+// let passwordGenerator = () => {
+//     // setting password length
+//     let length = 8;
+//     // creating character set
+//     let charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
 
-    // returned values
-    let retVal = "";
+//     // returned values
+//     let retVal = "";
 
 
-    // Looping over the character set and choosing pwd
-    for(let i = 0; i < length; i++){
-        n < charSet.length;
+//     // Looping over the character set and choosing pwd
+//     for(let i = 0; i < length; i++){
+//         n < charSet.length;
 
-        retVal += charSet.charAt(Math.floor(Math.random() * n))
+//         retVal += charSet.charAt(Math.floor(Math.random() * n))
 
-    }
+//     }
 
-    // returning the pwd generated
-    return retVal
-}
+//     // returning the pwd generated
+//     return retVal
+// }
